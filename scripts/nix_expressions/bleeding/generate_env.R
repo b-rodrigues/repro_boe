6d8cc6aed0269f841f6e9cc6f562f2f641e8e761
Bruno Rodrigues <bruno@brodrigues.co>
2025-01-28 15:28:19 +0100

    added gitignore

--- scripts/nix_expressions/bleeding/generate_env.R
+++
@@ -1,5 +0,0 @@
-library(rix)
-
-rix(r_ver = "bleeding_edge",
-    r_pkgs = c("dplyr", "ggplot2", "data.table"),
-    project_path = ".")
--- scripts/nix_expressions/rix_intro/generate_env_rstudio.R
+++ scripts/nix_expressions/rix_intro/generate_env_rstudio.R
@@ -5,10 +5,10 @@ rix(r_ver = "4.3.1",
     system_pkgs = NULL,
     git_pkgs = NULL,
     tex_pkgs = NULL,
-    # Attention, rstudio n'est pas disponible pour macOS
-    # via Nix, ni pour arm64-linux, seulement Linux et Windows
+    # Rstudio is not available for macOS through Nix
     ide = "rstudio",
-    # Ce shellhook est nécessaire pour démarrer Rstudio sur Linux
+    # This shell hook is necessary to start Rstudio
+    # on Linux
     shell_hook = "export QT_XCB_GL_INTEGRATION=none",
     project_path = ".",
     overwrite = TRUE)
